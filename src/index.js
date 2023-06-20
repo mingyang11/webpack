aba0cf8893f55421dac664e1fc7f5d780c764a85
yangmy <yangmy@servyou.com.cn>
2023-06-20 08:55:09 +0800

    fix: s

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import print from './demo';
+import imgsrc1 from '../assets/images/canvas.png';
+import imgsrc2 from '../assets/images/beijing.jpeg';
+import textDemo from '../assets/source/example.txt';
 
-(function (params) {
-  console.log(params);
-  return params;
-})(12324);
 print();
+
+const container = document.getElementById('id');
+const imgElement = document.createElement('img');
+imgElement.src = imgsrc1;
+container.appendChild(imgElement);
+
+const imgElement2 = document.createElement('img');
+imgElement2.src = imgsrc2;
+container.appendChild(imgElement2);
+
+const txtBlock = document.createElement('div');
+// txtBlock.style.cssText = 'width: 200px; height: 100px; background: red';
+txtBlock.textContent = textDemo;
+container.appendChild(txtBlock);
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,12 @@ module.exports = {
     filename: 'index.[hash:6].js',
     path: path.resolve(__dirname, 'build'),
     clean: true,
+    /**
+     * 资源文件配置,优先级低于在module中配置的优先级
+     * contenthash: 根据文件内容来形成hash
+     * ext: 文件拓展名
+     */
+    // assetModuleFilename: 'images/[contenthash][ext]',
   },
   mode: 'development',
   devtool: 'inline-source-map',
@@ -14,6 +20,45 @@ module.exports = {
     host: 'localhost',
     port: 3000,
     open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png)$/,
+        // 设置resource时图片资源会打包成文件，放在指定的文件路径中，可以设置文件路径和文件名
+        type: 'asset/resource',
+        /**
+         * 资源文件配置,优先级高于在output中配置的优先级
+         * contenthash: 根据文件内容来形成hash
+         * ext: 文件拓展名
+         */
+        generator: {
+          filename: 'images/[contenthash][ext]',
+        },
+      },
+      {
+        test: /\.jpg$/,
+        // 设置inline时图片资源会转化成base64码放在js文件中，此时不能设置文件路径和文件名
+        type: 'asset/inline',
+      },
+      {
+        test: /\.txt$/,
+        type: 'asset/source',
+      },
+      {
+        test: /\.jpeg$/,
+        // 通用资源类型，按照默认条件(8k)自动的在resource和inline中选择，否则会被视为resource类型
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 1024 * 1024 * 10,
+          },
+        },
+        generator: {
+          filename: 'images/[contenthash][ext]',
+        },
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
