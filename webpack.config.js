f4f36d31c3ebdfe4ac1a1d9b5f81cd676b0a78d5
yangmy <yangmy@servyou.com.cn>
2023-06-20 18:44:04 +0800

    asd

--- webpack.config.js
+++ webpack.config.js
@@ -62,7 +62,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './publick/index.html',
+      template: './public/index.html',
       filename: 'index.html',
       inject: 'body',
     }),
