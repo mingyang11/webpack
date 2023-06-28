316dfbd7bb34a354675b48ed870c88f263095771
yangmy <yangmy@servyou.com.cn>
2023-06-28 10:19:44 +0800

    feat: 添加提交信息校验功能

--- App.jsx
+++ App.jsx
@@ -1,10 +1,6 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 
 export default function App() {
-  return (
-    <div>
-      <h1>Hello Webpack-React</h1>
-    </div>
-  );
+  console.log(121);
+  return <div>hello</div>;
 }
