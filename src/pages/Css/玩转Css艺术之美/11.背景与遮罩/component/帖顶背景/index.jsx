6b9d497a1f8cb7fc40fb3654cc06cc98f5ee9620
yangmy <yangmy@servyou.com.cn>
2023-10-09 09:11:32 +0800

    fix: sa

--- src/pages/Css/玩转Css艺术之美/11.背景与遮罩/component/帖顶背景/index.jsx
+++ src/pages/Css/玩转Css艺术之美/11.背景与遮罩/component/帖顶背景/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './index.module.less';
 
 function Index() {
-  return <div className={styles.pastedBg}></div>;
+  return <div className={styles.pastedBg}>clip</div>;
 }
 
 export default Index;
